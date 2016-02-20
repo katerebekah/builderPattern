81fb06dcada11a7fbf0428aa6f3f8f2573a286ec
Kate <katerebekah@gmail.com>
2016-02-20 11:11:51 -0600

    added concrete class to build reports;

---
+++ builderPattern/Builders/ReportBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builderPattern.Builders
+{
+    class ReportBuilder : ABuilder
+    {
+        public override string BuildIntro()
+        {
+            return "";
+        }
+        public override string BuildConclusion()
+        {
+            return "";
+        }
+        public override string BuildBasicReport()
+        {
+            return "";
+        }
+        public override string BuildFullReportWithAccess()
+        {
+            return "";
+        }
+        public override string BuildFullReportWithoutAccess()
+        {
+            return "";
+        }
+    }
+}
