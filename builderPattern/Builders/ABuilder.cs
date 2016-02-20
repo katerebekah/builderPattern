b65716b11c2d87e7dfbd50f89d0c3943b5d2c277
Kate <katerebekah@gmail.com>
2016-02-20 11:07:47 -0600

    Added absract builder class

---
+++ builderPattern/Builders/ABuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builderPattern.Builders
+{
+    abstract class ABuilder
+    {
+        public abstract string BuildIntro();
+        public abstract string BuildConclusion();
+        public abstract string BuildBasicReport();
+        public abstract string BuildFullReportWithAccess();
+        public abstract string BuildFullReportWithoutAccess();
+    }
+}
